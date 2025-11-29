74213b06251d26af0221ca2be34045da760e8be0
ErosRJ <erosjimenez34@gmail.com>
2025-11-28 22:25:15 -0600

    Agrego calculadora científica modularizada

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+from calculator import ScientificCalculator
+
+if __name__ == '__main__':
+    app = ScientificCalculator()
+    app.mainloop()
---
+++ safe_globals.py
@@ -0,0 +1,18 @@
+# safe_globals.py
+import math
+
+SAFE_GLOBALS = {
+    'sin': math.sin,
+    'cos': math.cos,
+    'tan': math.tan,
+    'asin': math.asin,
+    'acos': math.acos,
+    'atan': math.atan,
+    'ln': math.log,
+    'log': math.log,
+    'exp': math.exp,
+    'e': math.e,
+    'pi': math.pi,
+    'sqrt': math.sqrt,
+    'pow': pow,
+}
